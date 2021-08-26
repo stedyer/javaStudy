68bc6719a1def33ec4c9f63e26bb21d79005deac
stedyer <86991095+stedyer@users.noreply.github.com>
2021-08-26 21:10:22 +0800

    Add files via upload

---
+++ RequestResult.java
@@ -0,0 +1,22 @@
+package com.example.myapp;
+
+public class RequestResult {
+    private int code;
+    private String text;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
